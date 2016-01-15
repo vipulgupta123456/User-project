2fcbe0a5d640ca938cc2406923b9698fb98a7066
Vipul <vipul@vipul.(none)>
2016-01-15 15:56:41 +0530

    changes

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
          :address                      => "smtp.gmail.com",
          :port                            => 587,
          :domain                      => "smtp.gmail.com",
-         :user_name                => "infyvipul13@gmail.com",
-         :password                  => "nittrichy",
+         :user_name                => "in",
+         :password                  => "sas",
          :authentication           => "plain",
          #:enable_starttls_auto  => true
          }
